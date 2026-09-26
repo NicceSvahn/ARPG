22a26b1ef7d1911992e3d7e34a279e2508b7be46
sebbe <sebbe.gerbert@gmail.com>
2026-09-26 13:41:59 +0200

    Fixed gamemode

--- Source/TestGame/Game/TestGameGameMode.h
+++ Source/TestGame/Game/TestGameGameMode.h
@@ -4,33 +4,10 @@
 #include "GameFramework/GameMode.h"
 #include "TestGameGameMode.generated.h"
 
-class ALevelGenerator;
-class AController;
 
 UCLASS()
 class TESTGAME_API ATestGameGameMode : public AGameMode
 {
 	GENERATED_BODY()
-	
-protected:
-    virtual void BeginPlay() override;
 
-    virtual void EndPlay(
-        const EEndPlayReason::Type EndPlayReason
-    ) override;
-
-    virtual void RestartPlayer(
-        AController* NewPlayer
-    ) override;
-
-private:
-    void HandleGeneratedMapReady();
-    bool HasGroundAtPlayerStart(AController* Player);
-
-    UPROPERTY()
-    TObjectPtr<ALevelGenerator> LevelGenerator = nullptr;
-
-    bool bMapReadyForPlayers = false;
-
-    TArray<TWeakObjectPtr<AController>> WaitingPlayers;
 };
